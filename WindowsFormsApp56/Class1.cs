711ad4fc1570bf2eecc7f12d59c04c8896771da4
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-02 21:06:30 +0330

    Add files via upload

---
+++ WindowsFormsApp56/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp56
+{
+ public class cGarbage
+   {
+        public static int count;
+
+        public cGarbage()
+        {
+            count += 1;
+        }
+        ~cGarbage()
+        {
+            count -= 1;
+        }
+
+        
+    }
+}
